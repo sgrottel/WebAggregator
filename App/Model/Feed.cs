4351f0394da874b8cadecc854040b2ad1a9994dd
sgrottel <webmaster@sgrottel.de>
2020-11-11 11:03:49 +0100

    Designing data model

---
+++ App/Model/Feed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Text;
+
+namespace SG.WebAggregator.Model
+{
+	class Feed
+	{
+		public string Title { get; set; }
+		public Url Link { get; set; }
+		public string Description { get; set; }
+
+		#region optionals
+		public object Icon { get; set; }
+		public object MetaData { get; set; }
+		public TimeSpan RefreshRate { get; set; }
+		#endregion
+	}
+}
