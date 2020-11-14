7974d5589d9634ba2291653097027f9c8bf79375
sgrottel <webmaster@sgrottel.de>
2020-11-14 09:40:36 +0100

    Started DataModel library

---
+++ DataModel/Article.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WebAggregator.Data
+{
+	/// <summary>
+	/// A news article
+	/// </summary>
+	public class Article
+	{
+		/// <summary>
+		/// Maschine-readable local unique id of the article
+		/// </summary>
+		public string Id { get; set; }
+
+		/// <summary>
+		/// Human-readable title of the article
+		/// </summary>
+		public string Title { get; set; }
+
+		/// <summary>
+		/// Referenced origin link of the article
+		/// </summary>
+		public Uri Link { get; set; }
+
+		/// <summary>
+		/// Last publication date or update date of the article
+		/// </summary>
+		public DateTime PublicationTime { get; set; }
+
+		/// <summary>
+		/// Formatted article content (HTML or MD or something)
+		/// </summary>
+		public string Content { get; set; }
+
+	}
+
+}
---
+++ DataModel/Feed.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAggregator.Data
+{
+	/// <summary>
+	/// A feed of news articles
+	/// </summary>
+	public class Feed
+	{
+
+		/// <summary>
+		/// Maschine-readable unique Id of the whole feed
+		/// </summary>
+		public string Id { get; set; }
+
+		/// <summary>
+		/// Human-readable title of the whole feed
+		/// </summary>
+		public string Title { get; set; }
+
+		/// <summary>
+		/// Referenced origin link of the whole feed
+		/// </summary>
+		public Uri Link { get; set; }
+
+		/// <summary>
+		/// Human-readable text description of the whole feed (no format support)
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Graphical favicon of the feed; At least 16x16 px in size. Additional sizes possible.
+		/// TODO: clarify format
+		/// </summary>
+		public object Icon { get; set; }
+
+		/// <summary>
+		/// Type id of the aggregator to use to fill the feed
+		/// </summary>
+		public string AggregatorType { get; set; }
+
+		/// <summary>
+		/// The source for the aggregator to collect the feed data, e.g. an uri
+		/// </summary>
+		public string AggregatorSource { get; set; }
+
+		/// <summary>
+		/// Update refresh rate to invoke the aggregator
+		/// </summary>
+		public TimeSpan AggregatorRate { get; set; }
+
+		/// <summary>
+		/// Generic settings for the aggregator
+		/// </summary>
+		public Dictionary<string, string> AggregatorSettings { get; set; }
+
+	}
+
+}
